5cd8eba7dccd4f0034379f5f8a0497608e1e33b2
kajal-patel97 <57909923+kajal-patel97@users.noreply.github.com>
2020-02-04 16:59:03 +0000

    ami

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,47 @@
 # Recipe:: default
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
+
+
+bash 'install_java' do
+  user 'root'
+  code <<-EOH
+  sudo apt update -y
+  sudo apt-get install default-jdk -y
+  EOH
+end
+
+# package 'default-jdk' do
+#   action :install
+# end
+#
+# package 'chefdk' do
+#   action :install
+# end
+
+
+# apt_update 'update_sources' do
+#   action :update
+# end
+#
+# execute 'java' do
+#   command 'sudo apt install default-jre -y'
+#   action :run
+# end
+#
+#
+# execute 'chefdk' do
+#   command 'sudo wget https://packages.chef.io/files/stable/chefdk/4.7.73/ubuntu/16.04/chefdk_4.7.73-1_amd64.deb'
+#   command 'sudo dpkg -i chefdk_3.2.30-1_amd64.deb'
+#   action :run
+# end
+#
+# # bash 'install_chef' do
+# #   user 'root'
+# #   code <<-EOH
+# #   wget https://packages.chef.io/files/stable/chefdk/4.7.73/ubuntu/16.04/chefdk_4.7.73-1_amd64.deb
+# #   sudo dpkg -i chefdk_3.2.30-1_amd64.deb
+# #   EOH
+# # end
+#
+#
--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -5,7 +5,7 @@
 
 unless os.windows?
   # This is an example test, replace with your own test.
-  describe user('root'), :skip do
+  describe user('root') do
     it { should exist }
   end
 end
@@ -13,4 +13,12 @@ end
 # This is an example test, replace it with your own test.
 describe port(80), :skip do
   it { should_not be_listening }
+end
+
+describe package 'java' do
+  it { should be_installed }
+end
+
+describe package 'chefdk' do 
+  it { should be_installed }
 end
